66d89b56047b9b6be4f47a90346862260b658d30
Jeff Sember <jpsember@gmail.com>
2023-01-14 13:15:26 -0800

    Issue #16

---
+++ src/main/java/js/testutil/parsing/StringParserTest.java
@@ -0,0 +1,85 @@
+package js.testutil.parsing;
+
+import static js.base.Tools.*;
+
+import org.junit.Test;
+
+import js.testutil.MyTestCase;
+import js.json.JSMap;
+import js.parsing.StringParser;
+
+public class StringParserTest extends MyTestCase {
+
+  @Test
+  public void peekIs() {
+    p("alpha bravo");
+    checkState(!p().peekIs("bravo"));
+    p().read("alpha ");
+    checkState(p().peekIs("bravo"));
+  }
+
+  @Test
+  public void readPath() {
+    auxReadPath("alpha/bravo/charlie.txt");
+  }
+
+  @Test
+  public void readPathWithEscaped() {
+    auxReadPath("\"alpha/bravo/charlie\\\"embedded\"");
+  }
+  
+
+  @Test
+  public void readInts() {
+    String inp=   "0 1 -0 -1 123 456 - alpha 5.5";
+    p(inp);
+    
+    JSMap result = map();
+    while (!p().done()) {
+      if (p().readIf(" ")) continue;
+      String res = "***";
+      String key;
+      try {
+        int x = p().readInteger();
+        res = ""+x;
+        key = res;
+      } catch (NumberFormatException e) {
+        StringBuilder z = new StringBuilder();
+        while (!p().done() && !p().peekIs(" "))
+          z.append(p().readChar());
+        key = z.toString();
+      }
+      result.put(key,res);
+    }
+    generateMessage(result);
+    log(result);
+    assertGenerated();
+  }
+
+  private StringParser p(String text) {
+    if (mStringParser == null)
+      mStringParser = new StringParser(text);
+    return mStringParser;
+  }
+
+  private void auxReadPath(String t) {
+    sb().append(p(t).readPath());
+    generateMessage(sb());
+    log(sb());
+    assertGenerated();
+  }
+
+  private StringParser p() {
+    checkState(mStringParser != null);
+    return mStringParser;
+  }
+
+  private StringParser mStringParser;
+  private StringBuilder msb;
+
+  private StringBuilder sb() {
+    if (msb == null)
+      msb = new StringBuilder();
+    return msb;
+  }
+}
