1c13989103601f478884bc2e48d546104edb511c
jeff <jeff@urbanlogiq.com>
2025-05-26 13:04:24 -0700

    ScannerTest unit test for new dfa parser

--- src/test/java/js/json/JSMapTest.java
+++ src/test/java/js/json/JSMapTest.java
@@ -1,18 +1,18 @@
 /**
  * MIT License
- * 
+ *
  * Copyright (c) 2021 Jeff Sember
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -20,7 +20,6 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
- * 
  **/
 package js.json;
 
@@ -33,6 +32,23 @@ import js.geometry.Matrix;
 import js.testutil.MyTestCase;
 
 public class JSMapTest extends MyTestCase {
+
+  @Test
+  public void parseMapWithComments() {
+    if (false) {
+      // Apparently my JSON parser doesn't allow comments?
+      var s = " # This is a comment\n { \"alpha\" : [1,2,3] } # another comment\n";
+      var mp = new JSMap(s);
+      pr(mp);
+    }
+  }
+
+  @Test
+  public void parseMapTrailingCommas() {
+    var s = " { \"alpha\" : [1,2,3,], } \n";
+    var mp = new JSMap(s);
+    assertMessage(mp);
+  }
 
   @Test
   public void prettyPrintTest() {
@@ -91,5 +107,5 @@ public class JSMapTest extends MyTestCase {
     JSMap m4 = map().put("m", m2);
     assertEquals(m3, m4);
   }
-  
+
 }
--- src/test/java/js/parsing/ScannerTest.java
+++ src/test/java/js/parsing/ScannerTest.java
@@ -4,7 +4,6 @@ package js.parsing;
 import static js.base.Tools.*;
 
 import js.base.BasePrinter;
-import js.file.Files;
 import org.junit.Test;
 
 import js.testutil.MyTestCase;
@@ -41,7 +40,27 @@ public class ScannerTest extends MyTestCase {
       p.pr("read:", s.read());
     }
     generateMessage(p.toString());
-//    assertMessage(p.toString());
+  }
+
+  @Test
+  public void compareOldNewDFAParser() {
+    var sampleDfa = "{\"final\":2,\"tokens\":[\"CR\",\"COMMA\",\"VALUE\"],\"version\":4.0,\"states\":[[[33,34,35,44,45,92,93,128],10,[44,45],9,[34,35],5,[32,33],4,[13,14],3,[10,11],1],[[-2,-1]],[],[[10,11],1],[[33,34,35,44,45,92,93,128],10,[-4,-3],2,[34,35],5,[32,33],4],[[32,34,35,92,93,255],5,[92,93],7,[34,35],6],[[-4,-3],2,[32,33],6],[[32,34,35,92,93,255],5,[92,93],7,[34,35],8],[[-4,-3],2,[33,34,35,92,93,255],5,[92,93],7,[32,33],8,[34,35],6],[[-3,-2]],[[32,34,35,44,45,92,93,128],10,[-4,-3]]]}";
+    var dfa1 = DFA.parseDfaFromJson(sampleDfa);
+    var dfa2 = new DFA(new JSMap(sampleDfa));
+    var out1 = scanSampleText(dfa1);
+    var out2 = scanSampleText(dfa2);
+    checkArgument(out1.equals(out2));
+    assertMessage(out1 + "\n\n" + out2);
+  }
+
+  private String scanSampleText(DFA dfa) {
+    var p = new BasePrinter();
+    var sampleText = "123,45,\"hello\",\n16";
+    var sc = new Scanner(dfa, sampleText, -1);
+    while (sc.hasNext()) {
+      p.pr("next token:", sc.read());
+    }
+    return p.toString();
   }
 
 }
