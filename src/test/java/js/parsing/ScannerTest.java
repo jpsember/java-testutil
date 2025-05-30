9e48ab6e2393fc5de78e8bcc7452ddca779947f2
jeff <jeff@urbanlogiq.com>
2025-05-30 15:21:53 -0700

    new dfa format

--- src/test/java/js/parsing/ScannerTest.java
+++ src/test/java/js/parsing/ScannerTest.java
@@ -18,8 +18,9 @@ public class ScannerTest extends MyTestCase {
   public Scanner sc(String text) {
     if (mScanner == null) {
       if (sDFA == null) {
-        var m = new JSMap("{\"final\":2,\"tokens\":[\"WS\",\"WORD\",\"SEMI\"],\"version\":4.0,\"states\":[[[9,11,12,14,32,33],4,[65,91,97,123],3,[59,60],1],[[-4,-3]],[],[[-3,-2],2,[65,91,97,123],3],[[9,11,12,14,32,33],4,[-2,-1]]]}");
-        sDFA = new DFA(m);
+       // var m = new JSMap("{\"final\":2,\"tokens\":[\"WS\",\"WORD\",\"SEMI\"],\"version\":4.0,\"states\":[[[9,11,12,14,32,33],4,[65,91,97,123],3,[59,60],1],[[-4,-3]],[],[[-3,-2],2,[65,91,97,123],3],[[9,11,12,14,32,33],4,[-2,-1]]]}");
+        sDFA = DFA.parse("\"{\\\"final\\\":2,\\\"tokens\\\":[\\\"WS\\\",\\\"WORD\\\",\\\"SEMI\\\"],\\\"version\\\":4.0,\\\"states\\\":[[[9,11,12,14,32,33],4,[65,91,97,123],3,[59,60],1],[[-4,-3]],[],[[-3,-2],2,[65,91,97,123],3],[[9,11,12,14,32,33],4,[-2,-1]]]}");
+
       }
       mScanner = new Scanner(sDFA, text);
     }
@@ -31,6 +32,7 @@ public class ScannerTest extends MyTestCase {
 
   @Test
   public void peekDistances() {
+    if (alert("disabled")) return;
     var s = sc();
     var p = new BasePrinter();
     while (s.hasNext()) {
@@ -42,16 +44,16 @@ public class ScannerTest extends MyTestCase {
     generateMessage(p.toString());
   }
 
-  @Test
-  public void compareOldNewDFAParser() {
-    var sampleDfa = "{\"final\":2,\"tokens\":[\"CR\",\"COMMA\",\"VALUE\"],\"version\":4.0,\"states\":[[[33,34,35,44,45,92,93,128],10,[44,45],9,[34,35],5,[32,33],4,[13,14],3,[10,11],1],[[-2,-1]],[],[[10,11],1],[[33,34,35,44,45,92,93,128],10,[-4,-3],2,[34,35],5,[32,33],4],[[32,34,35,92,93,255],5,[92,93],7,[34,35],6],[[-4,-3],2,[32,33],6],[[32,34,35,92,93,255],5,[92,93],7,[34,35],8],[[-4,-3],2,[33,34,35,92,93,255],5,[92,93],7,[32,33],8,[34,35],6],[[-3,-2]],[[32,34,35,44,45,92,93,128],10,[-4,-3]]]}";
-    var dfa1 = DFA.parseDfaFromJson(sampleDfa);
-    var dfa2 = new DFA(new JSMap(sampleDfa));
-    var out1 = scanSampleText(dfa1);
-    var out2 = scanSampleText(dfa2);
-    checkArgument(out1.equals(out2));
-    assertMessage(out1 + "\n\n" + out2);
-  }
+//  @Test
+//  public void compareOldNewDFAParser() {
+//    var sampleDfa = "{\"final\":2,\"tokens\":[\"CR\",\"COMMA\",\"VALUE\"],\"version\":4.0,\"states\":[[[33,34,35,44,45,92,93,128],10,[44,45],9,[34,35],5,[32,33],4,[13,14],3,[10,11],1],[[-2,-1]],[],[[10,11],1],[[33,34,35,44,45,92,93,128],10,[-4,-3],2,[34,35],5,[32,33],4],[[32,34,35,92,93,255],5,[92,93],7,[34,35],6],[[-4,-3],2,[32,33],6],[[32,34,35,92,93,255],5,[92,93],7,[34,35],8],[[-4,-3],2,[33,34,35,92,93,255],5,[92,93],7,[32,33],8,[34,35],6],[[-3,-2]],[[32,34,35,44,45,92,93,128],10,[-4,-3]]]}";
+//    var dfa1 = DFA.parseDfaFromJson(sampleDfa);
+//    var dfa2 = new DFA(new JSMap(sampleDfa));
+//    var out1 = scanSampleText(dfa1);
+//    var out2 = scanSampleText(dfa2);
+//    checkArgument(out1.equals(out2));
+//    assertMessage(out1 + "\n\n" + out2);
+//  }
 
   private String scanSampleText(DFA dfa) {
     var p = new BasePrinter();
